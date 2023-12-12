3bf749889c36e8a42a70cf7a2b47bb9b0ca53239
Chickenbuckit <s11896446@students.duvalschools.org>
2023-12-12 11:03:45 -0800

    v0.2

--- 06_OOP/06_OOP.py
+++ 06_OOP/06_OOP.py
@@ -1,4 +1,4 @@
-# object orented programming, Casey Boyce, v0.1
+# object orented programming, Casey Boyce, v0.2
 
 class Person: # use PASCALCASE for class_names 
     def __init__(self, name, age, weight):
@@ -10,4 +10,13 @@ class Person: # use PASCALCASE for class_names
     def __str__(self):
         return f"Name: {self.name}.\nThis person is {self.age} years old.\nThey weigh {self.weight}"
 person1 = Person('Chavna "The Brutalizer"', 34, 175)
+person2 = Person('Ur-Masi "The Massive"', 27, 547)
 print(person1)
+print(person2)
+
+if person1.weight > person2.weight:
+    print(f"{person1.name} weighs more than {person2.name}")
+elif person1.weight == person2.weight:
+    print("Each person weight the same.\n")
+else:
+    print(f"{person2.name} wieght more than {person1.name}")
