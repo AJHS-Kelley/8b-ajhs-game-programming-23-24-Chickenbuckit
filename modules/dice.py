bddca409fd95727089b2256c730ee6bc933fc3a1
Chickenbuckit <s11896446@students.duvalschools.org>
2023-12-14 10:42:08 -0800

    v0.1

---
+++ modules/dice.py
@@ -0,0 +1,22 @@
+# Dice Rolling mod, Casey Boyce, v0.1
+import random
+
+def desplay(num_dice, size_dice):
+    num_rolled = 0
+    sum = 0
+    while num_rolled < num_dice:
+        roll = random.randint(1, size_dice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum {sum}\n")
+        num_rolled += 1
+    return sum
+
+def roll(num_dice, size_dice):
+    num_rolled = 0
+    sum = 0
+    while num_rolled < num_dice:
+        roll = random.randint(1, size_dice)
+        sum += roll
+        num_rolled += 1
+    return sum
