c57df05db55eaf65f63e51b5e2a2c8a87b245a1c
Chickenbuckit <s11896446@students.duvalschools.org>
2023-12-14 11:24:29 -0800

    v0.1

--- modules/perfmon.py
+++ modules/perfmon.py
@@ -1,1 +1,13 @@
-#python performance monitoring modle, Casey Boyce, v0.0
+#python performance monitoring modle, Casey Boyce, v0.1
+import time
+
+def exec_start():
+    start_time = time.time()
+    return start_time
+
+def exec_stop():
+    stop_time = time.time()
+    return stop_time
+
+def exce_time(start_time, stop_time):
+    return f"Execution Time: {start_time - stop_time} Seconds.\n"
