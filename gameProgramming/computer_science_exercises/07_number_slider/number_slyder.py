bc448780ccbb6c9df15f150ad50ccd2eb02809ac
Chickenbuckit <s11896446@students.duvalschools.org>
2024-01-22 11:04:01 -0800

    v0.1

--- gameProgramming/computer_science_exercises/07_number_slider/number_slyder.py
+++ gameProgramming/computer_science_exercises/07_number_slider/number_slyder.py
@@ -1,1 +1,23 @@
-#number slyder, casey boyce, based on a project by Al Sweigart v0.0
+#number slyder, casey boyce, based on a project by Al Sweigart v0.1
+
+import sys, random, pygame
+#sys allows systems to access resources
+
+from pygame.locals import *
+
+
+# contants for game bord
+BOARD_WIDTH = 4 #collems
+BOARD_HIGHT = 4 #rows
+TILE_SIZE = 80 #pixel is the mesure ment
+WINDO_WITH = 640 #pixel is the mesure ment
+WINDO_HIGHT = 480 #pixel is the mesure ment
+FPS = 30 # max value will not snail run quicker
+BLANK = None
+
+# COLORS in (R,G,B) format
+BLAK = (0,0,0)
+WITE = (255,255,255)
+BRIGHBLUE = (0,50,255)
+DARKTORQOYS = (3,54,73)
+GREEN = (0,204,0)
