9e2ac206b626d343bb69ede520e1c87d500411fb
Chickenbuckit <s11896446@students.duvalschools.org>
2024-02-05 12:28:22 -0800

    v0.5

--- gameProgramming/computer_science_exercises/08_ultimate_intro_pygame/ultimate_intro_pygame.py
+++ gameProgramming/computer_science_exercises/08_ultimate_intro_pygame/ultimate_intro_pygame.py
@@ -3,11 +3,10 @@ import pygame
 from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.dsplay.set_caption('Runner')
+pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+test_surface = pygame.image.load('Pictures/images.png')
 
 while True:
     for event in pygame.event.get():
@@ -15,6 +14,6 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface(0, 0))
+    screen.blit(test_surface, (0, 0))
     pygame.display.update()
     clock.tick(60)
