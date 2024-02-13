8cb55072e8e13f55fa6dcc7c65e30161597d0a66
Chickenbuckit <s11896446@students.duvalschools.org>
2024-02-13 12:21:55 -0800

    v0.6

--- gameProgramming/computer_science_exercises/08_ultimate_intro_pygame/ultimate_intro_pygame.py
+++ gameProgramming/computer_science_exercises/08_ultimate_intro_pygame/ultimate_intro_pygame.py
@@ -6,14 +6,13 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-test_surface = pygame.image.load('img\gross.jpg')
-
+grond_surface = pygame.image.load('img\gross.jpg')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface, (0, 0))
+    screen.blit(grond_surface, (0, 0))
     pygame.display.update()
     clock.tick(60)
