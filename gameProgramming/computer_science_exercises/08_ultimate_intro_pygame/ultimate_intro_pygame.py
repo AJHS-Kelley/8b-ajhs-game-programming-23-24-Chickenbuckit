9024e995295887b45d0ebc2ff906b9f6b35c7804
Chickenbuckit <s11896446@students.duvalschools.org>
2024-02-13 12:41:12 -0800

    v0.2

--- gameProgramming/computer_science_exercises/08_ultimate_intro_pygame/ultimate_intro_pygame.py
+++ gameProgramming/computer_science_exercises/08_ultimate_intro_pygame/ultimate_intro_pygame.py
@@ -1,12 +1,16 @@
-# ultimate intro into pygame casey boyce v0.1
+# ultimate intro into pygame casey boyce v0.2
 import pygame
 from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("comicsansms", 50)
 
-grond_surface = pygame.image.load('img\gross.jpg')
+sky_box = pygame.image.load('img\download.jpg')
+grond_surface = pygame.image.load('img\gross.png')
+text_surface = test_font.render("Casey's game", False, 'black')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -14,5 +18,7 @@ while True:
             exit()
 
     screen.blit(grond_surface, (0, 0))
+    screen.blit(sky_box, (0, 300))
+    screen.blit(text_surface, (300, 50))
     pygame.display.update()
     clock.tick(60)
