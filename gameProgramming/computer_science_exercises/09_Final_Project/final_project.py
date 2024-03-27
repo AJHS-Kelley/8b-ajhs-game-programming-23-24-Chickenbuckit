0982a76ef00f84c9c16482b353260695e68dc555
Chickenbuckit <s11896446@students.duvalschools.org>
2024-03-27 14:45:32 -0800

    0.1

--- gameProgramming/computer_science_exercises/09_Final_Project/final_project.py
+++ gameProgramming/computer_science_exercises/09_Final_Project/final_project.py
@@ -1,2 +1,19 @@
 #Casey Boyce, Final Project, 0.1
 import time, pygame, sys
+resilution = 0 # 0 for low. 1 for high
+
+if resilution == 0:
+    x = 800
+    y = 600
+else:
+    x = 1920
+    y = 1080
+
+screen = pygame.display.set_mode((x,y))
+
+difficulty = input("Please chose a Difficulty Easy or Hard.\n").capitalize
+
+if difficulty == "EASY":
+    pygame.display.set_caption('Jumpin To Da Top -- Grandmama Mode')
+else:
+    pygame.display.set_caption("Why Did You Pick This Mode")
