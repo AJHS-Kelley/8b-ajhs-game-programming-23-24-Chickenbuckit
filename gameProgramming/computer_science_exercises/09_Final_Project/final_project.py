14ca45b65d88524f8519340b12016236adeea547
Chickenbuckit <s11896446@students.duvalschools.org>
2024-04-11 14:50:30 -0800

    Drawing done (mostly)

--- gameProgramming/computer_science_exercises/09_Final_Project/final_project.py
+++ gameProgramming/computer_science_exercises/09_Final_Project/final_project.py
@@ -1,22 +1,29 @@
 #Casey Boyce, Final Project, 0.1
-import time, pygame, sys
+import time, pygame, sys, os
 
-resilution = 0 # 0 for low. 1 for high
-
-if resilution == 0:
+resilution = input('\nresilution:\n    0 for 800 by 600\n    1 for 1920 by 1080 (Defalt)\n') # 0 for low. 1 for high
+a = 20
+z = 5
+if resilution == '0':
     x = 800
     y = 600
 else:
     x = 1920
     y = 1080
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (z,a)
 
 screen = pygame.display.set_mode((x,y))
 start_time = pygame.time.get_ticks()
-difficulty = input("\n\nPlease chose a Difficulty Easy or Hard.\n\n\nDefalt is Easy  ").lower
+difficulty = input("\n\nPlease chose a Difficulty Easy or Hard.\nDefalt is Easy\n\n").lower()
 
 if difficulty == "hard":
     pygame.display.set_caption("Leaping Ruler -- Dont Fall Now ")
 else:
     pygame.display.set_caption("Leaping Ruler -- Baby Mode ")
 
-time.sleep(5)
+start = pygame.image.load('gameProgramming/IMG/bg water/Start.png').convert()
+
+screen.blit(start, (0, 0))
+pygame.display.update()
+
+time.sleep(10)
